ef7dc1cebcd1e07245b0937250d1dc120c6e84bb
Sumedh Kumar <manos01@pfw.edu>
2023-12-04 15:28:10 -0500

    added a null check for new skills

--- ProFolio-Backend/src/main/java/com/profolio/portfoliobuilder/services/SkillService.java
+++ ProFolio-Backend/src/main/java/com/profolio/portfoliobuilder/services/SkillService.java
@@ -22,20 +22,22 @@ public class SkillService {
 
     public Set<Skill> modifySkillList(User user, Set<Skill> newSkills) {
         Set<Skill> existingSkills = user.getSkills();
-        Set<Skill> updatedSkills = new HashSet<>();
 
-        for (Skill skill : newSkills) {
-            Optional<Skill> optionalSkill = skillRepository.findByName(skill.getName());
-            if (optionalSkill.isPresent()) {
-                updatedSkills.add(optionalSkill.get());
-            } else {
-                Skill savedSkill = skillRepository.save(skill);
-                updatedSkills.add(savedSkill);
+        if (newSkills != null && !newSkills.isEmpty()) {
+            Set<Skill> updatedSkills = new HashSet<>();
+            for (Skill skill : newSkills) {
+                Optional<Skill> optionalSkill = skillRepository.findByName(skill.getName());
+                if (optionalSkill.isPresent()) {
+                    updatedSkills.add(optionalSkill.get());
+                } else {
+                    Skill savedSkill = skillRepository.save(skill);
+                    updatedSkills.add(savedSkill);
+                }
             }
+            existingSkills.clear();
+            existingSkills.addAll(updatedSkills);
         }
-
-        existingSkills.clear();
-        existingSkills.addAll(updatedSkills);
+        
         return existingSkills;
     }
 }
