17c69e2a2e2c83e8d87d1944b40c7b8f43f53c47
Aditya <62930521+adityar224@users.noreply.github.com>
2023-12-06 13:17:02 -0500

    removed left margin

--- ProFolio-Frontend/src/components/Template 3/EducationCard.jsx
+++ ProFolio-Frontend/src/components/Template 3/EducationCard.jsx
@@ -8,14 +8,11 @@ export default function EducationCard({ data }) {
     <Col lg="6">
       <Card className="card shadow-lg p-3 mb-5 bg-white rounded">
         <Card.Body>
-          <Row>
-            <Card.Title as="h4" className="col-12 col-md-8">{data.institutionName}</Card.Title>
-            <div className="col-6 col-md-4 text-muted">{data.location}</div>
-          </Row>
-          <Row>
-            <Card.Subtitle as="h5" className="col-12 col-md-8">{data.degreeName}</Card.Subtitle>
-            <div className="col-6 col-md-4 text-muted">{data.fromDate} - {data.toDate}</div>
-          </Row>
+        <div className="col-6 col-md-4 text-muted no-left-margin">{data.fromDate} to {data.toDate}</div>
+            <Card.Title as="h4" className="col-12 col-md-8 no-left-margin">{data.institutionName}</Card.Title>
+            <Card.Subtitle as="h5" className="col-12 col-md-8 no-left-margin">{data.degreeName}</Card.Subtitle>
+            <Card.Subtitle as="h6" className="col-12 col-md-8 no-left-margin">{data.location}</Card.Subtitle>
+            <hr />
           <Card.Text>{data.description}</Card.Text>
         </Card.Body>
       </Card>
